dcb88d251d9193e09df73d05587c97028e918e41
muro1214 <s.muro.1214@gmail.com>
2021-03-07 18:54:27 +0900

    v0.0.4

--- AutomaticDoorMod/AutomaticDoorMod.cs
+++ AutomaticDoorMod/AutomaticDoorMod.cs
@@ -15,11 +15,15 @@ namespace AutomaticDoorMod
 
         public static ConfigEntry<bool> isEnabled;
         public static ConfigEntry<float> waitForDoorToCloseSeconds;
+        public static ConfigEntry<string> toggleSwitchModKey;
+        public static ConfigEntry<string> toggleSwitchKey;
 
         private void Awake()
         {
             isEnabled = Config.Bind<bool>("General", "IsEnabled", true, "If you set this to false, this mod will be disabled.");
             waitForDoorToCloseSeconds = Config.Bind<float>("General", "waitForDoorToCloseSeconds", 5, "Specify the time in seconds to wait for the door to close automatically.");
+            toggleSwitchModKey = Config.Bind<string>("General", "toggleSwitchModKey", "left alt", "Specifies the MOD Key of toggleSwitchKey. If left blank, it is not used.");
+            toggleSwitchKey = Config.Bind<string>("General", "toggleSwitchKey", "f10", "Toggles between enabled and disabled mods when this key is pressed.");
 
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
         }
@@ -28,11 +32,14 @@ namespace AutomaticDoorMod
         public static class AutomaticDoor
         {
             public static bool isInsideCrypt = false;
+            public static bool toggleSwitch = true;
 
-            private static void Postfix(ref Door __instance, ZNetView ___m_nview, ref ItemDrop ___m_keyItem)
+            private static void Postfix(ref Door __instance, ZNetView ___m_nview)
             {
-                // ___m_keyItem: CryptKey
-                if (!AutomaticDoorModPlugin.isEnabled.Value || ___m_keyItem != null || isInsideCrypt)
+                if (!AutomaticDoorModPlugin.isEnabled.Value || // when mod is disabled
+                    __instance.m_keyItem != null || // when target door needs keyItem (e.g. CryptKey)
+                    isInsideCrypt || // when player is in Crypt
+                    !toggleSwitch) // when a player manually disables a mod
                 {
                     return;
                 }
@@ -54,29 +61,99 @@ namespace AutomaticDoorMod
             }
         }
 
-        [HarmonyPatch(typeof(EnvMan), "SetForceEnvironment")]
-        public static class SwitchModEnable
+        [HarmonyPatch(typeof(EnvZone), "OnTriggerStay")]
+        public static class DisableModWhenCryptStay
         {
-            private static void Prefix(string env, ref string ___m_forceEnv)
+            private static string currentEnv = "";
+
+            private static void Prefix(Collider collider, ref EnvZone __instance)
             {
                 if (!AutomaticDoorModPlugin.isEnabled.Value)
                 {
                     return;
                 }
 
-                if(___m_forceEnv == env)
+                if(currentEnv == __instance.m_environment)
                 {
                     return;
                 }
 
-                AutomaticDoor.isInsideCrypt = env.Contains("Crypt");
-                if (AutomaticDoor.isInsideCrypt)
+                Player component = collider.GetComponent<Player>();
+                if(component == null || Player.m_localPlayer != component)
                 {
-                    Debug.Log("MOD disabled because player has entered Crypt.");
+                    return;
                 }
-                else
+                
+                if (__instance.m_environment.Contains("Crypt"))
                 {
-                    Debug.Log("MOD enabled because player has exited Crypt.");
+                    AutomaticDoor.isInsideCrypt = true;
+                    currentEnv = __instance.m_environment;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(EnvZone), "OnTriggerExit")]
+        public static class EnableModWhenCryptExit
+        {
+            private static void Prefix(Collider collider, ref EnvZone __instance)
+            {
+                if (!AutomaticDoorModPlugin.isEnabled.Value)
+                {
+                    return;
+                }
+
+                Player component = collider.GetComponent<Player>();
+                if (component == null || Player.m_localPlayer != component)
+                {
+                    return;
+                }
+
+                AutomaticDoor.isInsideCrypt = false;
+            }
+        }
+
+        [HarmonyPatch(typeof(Player), "Update")]
+        public static class ToggleSwitch
+        {
+            private static bool isToggleKeyPressed()
+            {
+                string modKey = AutomaticDoorModPlugin.toggleSwitchModKey.Value.ToLower();
+                string key = AutomaticDoorModPlugin.toggleSwitchKey.Value.ToLower();
+
+                if(modKey.Equals("") || Input.GetKey(modKey))
+                {
+                    if (Input.GetKeyDown(key))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            private static void Postfix(Player __instance)
+            {
+                if (!AutomaticDoorModPlugin.isEnabled.Value)
+                {
+                    return;
+                }
+
+                if (Player.m_localPlayer != __instance)
+                {
+                    return;
+                }
+
+                if (isToggleKeyPressed())
+                {
+                    AutomaticDoor.toggleSwitch = !AutomaticDoor.toggleSwitch;
+                    if (AutomaticDoor.toggleSwitch)
+                    {
+                        MessageHud.instance.ShowMessage(MessageHud.MessageType.TopLeft, "Automatic Door Mod: Enabled");
+                    }
+                    else
+                    {
+                        MessageHud.instance.ShowMessage(MessageHud.MessageType.TopLeft, "Automatic Door Mod: Disabled");
+                    }
                 }
             }
         }
--- README.md
+++ README.md
@@ -10,10 +10,14 @@ This is a mod that automatically closes the door that the player has opened afte
 3. Copy `AutomaticDoorMod.dll` to `<Valheim Root Dir>\BepInEx\plugins`
 
 ## Configuration
-Please edit `.\BepInEx\config\muro1214.valheim_mods.automatic_door.cfg`.
+Please edit `.\BepInEx\config\muro1214.valheim_mods.automatic_door.cfg`.  
+
+Default hot key is LeftAlt + F10.  
+Hot key format is as follows.  
+https://docs.unity3d.com/Manual/ConventionalGameInput.html
 
 ~~~ini
-## Settings file was created by plugin Automatic Door Mod v0.0.1
+## Settings file was created by plugin Automatic Door Mod v0.0.4
 ## Plugin GUID: muro1214.valheim_mods.automatic_door
 
 [General]
@@ -27,7 +31,20 @@ IsEnabled = true
 # Setting type: Single
 # Default value: 5
 waitForDoorToCloseSeconds = 5
+
+## Specifies the MOD Key of toggleSwitchKey. If left blank, it is not used.
+# Setting type: String
+# Default value: left alt
+toggleSwitchModKey = left alt
+
+## Toggles between enabled and disabled mods when this key is pressed.
+# Setting type: String
+# Default value: f10
+toggleSwitchKey = f10
 ~~~
 
 ## Changelog
 * v0.0.1: Initial Release
+* v0.0.2: Disable mod when player is in SunkenCrypt
+* v0.0.3: Disable mod when player is in Crypt -> support for both Forest Crypt and Sunken Crypt
+* v0.0.4: Added hotkeys to enable and disable mod
